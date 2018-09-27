a75f8f336cbf2f963ec9d73401cf5fc04b5b4183
Jacob Menashi <jbmenashi@gmail.com>
2018-09-27 14:54:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,10 +6,7 @@ def prime?(number)
   else
     range_array = (2...number).to_a 
     range_array.each do |element|
-      if number % element == 0 
-        return false
-      end
-    end 
-    return true
+      number % element == 0 ? false : true
+    end
   end
 end
